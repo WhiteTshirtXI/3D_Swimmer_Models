0b3a453e80586acd93d26a0308eae66a66d13962
kbroe <kellenbroemmelsiek@gmail.com>
2016-08-08 18:39:28 -0700

    Explicit Solver 3D VE: Stress Tensor Plot

--- Explicit_Solver_3D_Restrict_2D_Viscoelastic/plot_data.m
+++ Explicit_Solver_3D_Restrict_2D_Viscoelastic/plot_data.m
@@ -3,12 +3,12 @@ clear
 load('./data/imworm_3D_VE_n128_t3.000000.mat');
     X1 = XTworm;
     U1 = U;
-    S1 = Shat;
+    Sh1 = Shat;
 % Load second data set
-load('./data/imworm_3D_VE_n128_t2.900000.mat');
+load('./data/imworm_3D_VE_n128_t3.000000.mat');
     X2 = XTworm;
     U2 = U;
-    S2 = Shat;
+    Sh2 = Shat;
 % Get worm statistics
 [MAV1, F1, XCM1] = get_speed(X1);
 [MAV2, F2, XCM2] = get_speed(X2);
@@ -57,4 +57,33 @@ load('./data/imworm_3D_VE_n128_t2.900000.mat');
     title('Center of Mass');
     xlabel('Time in .01 secs');
     ylabel('Z-coordinate');
-% To-Do : Bending Energy L2-Norm, Stress Tensor Isoperimetric Surface
+% To-Do : Bending Energy L2-Norm
+
+for i = 1:6
+    S1(:,:,:,i) = real(ifftn(Sh1(:,:,:,i)));
+end
+
+trs = S1(:,:,:,1) + S1(:,:,:,4) + S1(:,:,:,6);
+% Note: Adjust increment value to increase plot slice density
+Sx = -1:0.2:1;
+Sy = -1:0.2:1;
+Sz = -1:0.2:1;
+cvals = linspace(-1,1,128);
+[X,Y,Z] = meshgrid(linspace(-1,1,128),linspace(-1,1,128),linspace(-1,1,128));
+
+% First 3-D Figure (Worm Focus)
+figure
+contourslice(X,Y,Z,trs,Sx,Sy,Sz,cvals);
+axis([-1,1,-1,1,-1,1]);
+daspect([1,1,1]);
+campos([10,-20,10]);
+title('Worm View');
+box on
+% Second 3-D Figure (Full Focus)
+figure
+contourslice(X,Y,Z,trs,Sx,Sy,Sz);
+axis([-1,1,-1,1,-1,1]);
+daspect([1,1,1]);
+campos([10,-20,10]);
+title('Full View');
+box on
