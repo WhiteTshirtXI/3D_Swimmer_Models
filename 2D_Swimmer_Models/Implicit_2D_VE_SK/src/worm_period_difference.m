2c4e287b2185f8719a4bca8d9fd9d2835aebd86f
kbroe <kellenbroemmelsiek@gmail.com>
2016-09-14 13:52:25 -0700

    Removed 2D Soft Kicker Model
    Model is a subset of the kicker & burrower model commit in
    7af8af2b2989fffee9dbea4c26a6b050602d9b7b

--- 2D_Swimmer_Models/Implicit_2D_VE_SK/src/worm_period_difference.m
+++
@@ -1,5 +0,0 @@
-function [absDiff] = worm_period_difference(XTworm, Tperstep)
-endStep = size(XTworm,3);
-absDiff = abs(getPeriodAverage(XTworm, Tperstep) - getPeriodAverage(XTworm(:,:,1:endStep - Tperstep), Tperstep));
-end
-
